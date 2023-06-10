b04e298539b79a7e00b7c0cec75f68a295ea93e8
Yuvin <yuvin@agrithmics.lk>
2023-06-10 16:12:33 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Reg from'./Modules/Registration/Reg'
+function App() {
+  return (
+    <div className="App">
+      <Reg/>
+    </div>
+  );
+}
+
+export default App;
